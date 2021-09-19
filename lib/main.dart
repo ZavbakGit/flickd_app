50befe34619027181bb920c22aae3b049700dff8
ZavbakGit <f%jtVPW3hbmjy!>
2021-09-20 08:47:24 +1000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flickd_app/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    SplashPage(
+      key: UniqueKey(),
+      onInitializationComplete: () => {},
+    ),
+  );
+}
