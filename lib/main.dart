f76eb2181dd260704d70cf326df14deb013266cc
ZavbakGit <f%jtVPW3hbmjy!>
2021-09-20 13:45:32 +1000

    start

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flickd_app/pages/main_page.dart';
 import 'package:flickd_app/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -5,7 +6,27 @@ void main() {
   runApp(
     SplashPage(
       key: UniqueKey(),
-      onInitializationComplete: () => {},
+      onInitializationComplete: () => runApp(MyApp()),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flickd',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext _context) => const MainPage(),
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
 }
