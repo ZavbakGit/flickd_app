05680ac2c895bfb13d70dab59a6d1d5ce3b03cd3
ZavbakGit <f%jtVPW3hbmjy!>
2021-09-22 15:44:09 +1000

    finished 17-

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -9,10 +9,15 @@ class MainPage extends ConsumerWidget {
   double? _deviceHeight;
   double? _deviceWidth;
 
+  TextEditingController? _searchTextFieldController;
+
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
+
+    _searchTextFieldController = TextEditingController();
+
     return buildUI();
   }
 
@@ -41,7 +46,7 @@ class MainPage extends ConsumerWidget {
         borderRadius: BorderRadius.circular(10.00),
         image: const DecorationImage(
             image: NetworkImage(
-                'https://cdn.pixabay.com/photo/2016/10/20/18/35/earth-1756274_960_720.jpg'),
+                'https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'),
             fit: BoxFit.cover),
       ),
       child: BackdropFilter(
@@ -61,7 +66,7 @@ class MainPage extends ConsumerWidget {
       width: _deviceHeight! * 0.88,
       child: Column(
         mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           _topBarWidget(),
@@ -73,25 +78,40 @@ class MainPage extends ConsumerWidget {
   Widget _topBarWidget() {
     return Container(
       height: _deviceHeight! * 0.08,
-      color: Colors.black54,
       decoration: BoxDecoration(
-          color: Colors.black54, borderRadius: BorderRadius.circular(20.0)),
+        color: Colors.black54,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
       child: Row(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          _searchFieldWidget()
+          _searchFieldWidget(),
         ],
       ),
     );
   }
 
- Widget _searchFieldWidget() {
+  Widget _searchFieldWidget() {
+    final _border = InputBorder.none;
     return Container(
-      width: _deviceWidth!*0.50,
-      height: _deviceHeight!*0.05,
-      child: TextField,
+      width: _deviceWidth! * 0.50,
+      height: _deviceHeight! * 0.05,
+      child: TextField(
+        controller: _searchTextFieldController,
+        onSubmitted: (_input) {},
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+          focusedBorder: _border,
+          border: _border,
+          prefixIcon: Icon(Icons.search, color: Colors.white24),
+          hintStyle: TextStyle(color: Colors.white54),
+          filled: false,
+          fillColor: Colors.white24,
+          hintText: 'Search...',
+        ),
+      ),
     );
- }
+  }
 }
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -25,7 +25,7 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 5))
+    Future.delayed(const Duration(seconds: 1))
         .then((_) => _setup(context))
         .then(
           (_) => widget.onInitializationComplete!(),
