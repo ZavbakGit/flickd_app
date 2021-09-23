3576019725bfebb6758743d5d1120db109cfde24
ZavbakGit <f%jtVPW3hbmjy!>
2021-09-23 10:08:35 +1000

    finished 27

--- lib/services/movie_service.dart
+++ lib/services/movie_service.dart
@@ -1,12 +1,31 @@
+
+import 'package:dio/dio.dart';
+import 'package:flickd_app/models/movie.dart';
 import 'package:flickd_app/services/http_service.dart';
 import 'package:get_it/get_it.dart';
 
-class MovieService{
+class MovieService {
   final GetIt getIt = GetIt.instance;
 
   HTTPService? _http;
 
-  MovieService(){
+  MovieService() {
     _http = getIt.get<HTTPService>();
+  }
+
+  Future<List<Movie>> getPopularMovie({int page = 1}) async {
+    Response? _response = await _http!.get('/movie/popular', query: {
+      'page': page,
+    });
+
+    if(_response!.statusCode == 200){
+      Map _data = _response.data;
+      List<Movie> _movies = _data['results'].map<Movie>((_movieData){
+        return Movie.from(_movieData);
+      }).toList();
+      return _movies;
+    }else{
+      throw Exception('Couldn\'t load popular movies');
+    }
   }
 }
